f92793efbc49807169bed78fda7b5696a8a70384
Guillem <gody08112002@gmail.com>
2022-12-19 18:22:48 +0100

    addition of the cron.sh file for the scheduled functions

--- cgi-bin/cron.sh
+++ cgi-bin/cron.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+echo -e "Content-type: text/plain\n"
+IN=$(cat)
 
+if [ -n "$IN" ] #If IN is not empty
+then
+  #Add new programmed task
+  (echo "$IN" | cut -d "&" -f1) >> /etc/
+
+  #Delete existing programmed task
+else
+  sudo fcrontab -l #Shows all the scheduled tasks by cron
+fi
---
+++ cgi-bin/manage-users.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e "Content-type: text/plain\n"
